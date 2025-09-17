ff9b427b51b9b9c8ac3fd938db0017a19807f968
Adam Sidhoum <adamsidhoum@MacBook-Air-de-Adam-2.local>
2025-09-17 16:42:49 +0200

    Correction de l'inscription backend

--- server/Controllers/User.js
+++ server/Controllers/User.js
@@ -5,16 +5,29 @@ const jwt = require('jsonwebtoken');
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
-        const user = new User ({
+        const user = new User({
             email: req.body.email,
-        password: hash
+            password: hash
         });
         user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+        .then(() => {
+            // Génération du token directement après création
+            const token = jwt.sign(
+                { userId: user._id },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+            );
+            res.status(201).json({
+                userId: user._id,
+                token: token,
+                message: 'Utilisateur créé !'
+            });
+        })
         .catch(error => res.status(400).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
 };
+
 
 
 exports.login = (req, res, next) => {
